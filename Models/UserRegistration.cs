adba68777fa1ea663249041f10b6a5f3ecdda1c4
MariaMuhammadAshraf <168551832+MariaMuhammadAshraf@users.noreply.github.com>
2025-09-02 12:03:13 +0500

    Update UserRegistration.cs

--- Models/UserRegistration.cs
+++ Models/UserRegistration.cs
@@ -8,19 +8,20 @@ public partial class UserRegistration
 {
     public int UserId { get; set; }
 
-    public string FirstName { get; set; } = null!;
+    public string? FirstName { get; set; }
 
-    public string LastName { get; set; } = null!;
+    public string? LastName { get; set; }
 
     [Required(ErrorMessage = "User name is required.")]
     public string UserName { get; set; } = null!;
 
     [Required(ErrorMessage = "Email is required.")]
     [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Please enter a valid email address.")]
+
     public string UserEmail { get; set; } = null!;
 
-
-    public string UserPassword { get; set; } = null!;
+    public string? UserPassword { get; set; }
 
     public string? UserRole { get; set; }
+
 }
